3f88917fa5fe971d9c352888fb9db6be01235659
philippashenkov <61788331+philippashenkov@users.noreply.github.com>
2020-03-30 23:56:46 +0300

    add buttons 'More Info'

--- YOGA/js/script.js
+++ YOGA/js/script.js
@@ -84,4 +84,32 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descrBtn = document.querySelectorAll(".description-btn");
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+        
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    descrBtn.forEach(tab => {
+        tab.addEventListener('click',function(){
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
 });
