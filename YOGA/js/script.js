1de0ff7bae2b015989463e5e3b9c4fc0d70c7532
philippashenkov <61788331+philippashenkov@users.noreply.github.com>
2020-04-03 22:00:04 +0300

    add request form

--- YOGA/js/script.js
+++ YOGA/js/script.js
@@ -112,4 +112,54 @@ window.addEventListener('DOMContentLoaded', function() {
             document.body.style.overflow = 'hidden';
         });
     });
+
+    // FOrm
+
+    let message = {
+        loading: 'Загрузка',
+        success: 'Скоро мы с ввами свяжемся',
+        failure: 'Чтото пошло не так'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit',function(evemt) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded'); вариант с обычной формой,ниже чрез формат JSON
+
+        request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        // request.send(formData); 
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            }else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            }else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i =0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 });
